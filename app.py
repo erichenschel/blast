74468c5a07ab725c0c82b2084b864adb7b1d642f
Eric <eric.henschel7@gmail.com>
2020-04-28 16:36:29 -0400

    dashboard

---
+++ app.py
@@ -0,0 +1,17 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+#app = dash.Dash
+
+app.layout = html.Div(children=[
+    html.H1(children='Blast Simulation')
+
+
+    
+    
+    
+    
+    
+    
+    ]
